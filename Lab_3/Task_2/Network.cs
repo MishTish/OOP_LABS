347c2e1ab6ddb6e39936c81fb0c25bda231c07fa
MishTish <mishka.tish@gmail.com>
2024-10-19 14:16:43 +0300

    L3T2

---
+++ Lab_3/Task_2/Network.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_2
+{
+    internal class Network
+    {
+        private List<Computer> devices;
+
+
+        public Network()
+        {
+            devices = new List<Computer>();
+        }
+
+
+        public void AddDevice(Computer device)
+        {
+            devices.Add(device);
+            Console.WriteLine($"{device.GetType().Name} added to the network.");
+        }
+        public void RemoveDevice(Computer device)
+        {
+            devices.Remove(device);
+            Console.WriteLine($"{device.GetType().Name} removed from the network.");
+        }
+
+
+        public void TransferData(Computer sender, Computer reciever, string data)
+        {
+            sender.SendData(reciever, data);
+        }
+
+
+        public void ConnectDevices(Computer device1, Computer device2)
+        {
+            device1.Connect(device2);
+            device2.Connect(device1);
+        }
+        public void DisconnectDevices(Computer device1, Computer device2)
+        {
+            device1.Disconnect(device2);
+            device2.Disconnect(device1);
+        }
+    }
+}
---
+++ Lab_3/Task_2/Workstation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Workstation : Computer
+    {
+        public string UserName { get; set; }
+
+        public Workstation(string ipAddress, int outputPower, string osType, string userName) : base(ipAddress, outputPower, osType)
+        {
+            UserName = userName;
+        }
+
+
+    }
+}
